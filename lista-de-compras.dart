64be43f3df9147353d5718b817ec16b2483b64e0
elieber-fernandes <57969241+elieber-fernandes@users.noreply.github.com>
2020-10-12 22:57:26 -0300

    Update lista-de-compras.dart
    melhorias.

--- lista-de-compras.dart
+++ lista-de-compras.dart
@@ -6,21 +6,25 @@ main() {
   bool condicao = true;
 
   while (condicao) {
-    print('### LISTA DE COMPRA ###\n Digite os produtos: \n');
+    print('### LISTA DE COMPRA ###\n Digite os produtos:');
     String text = stdin.readLineSync();
     if (text == '1') {
       print('Programa finalizado!');
       condicao = false;
     } else if (text == '2') {
-      print('$produtos\n');
+      for (var i = 0; i < produtos.length; i++) {
+        print('ITEM $i - ${produtos[i]}');
+      }
     } else if (text == '3') {
-      print('Qual item você quer remover?');
+      print('Digite o NUMERO do item para remover?');
       String remover = stdin.readLineSync();
-      produtos.remove(remover);
+      int numRemover = int.parse(remover);
+      produtos.removeAt(numRemover);
     } else {
       produtos.add(text);
+      print('\x1B[2J\x1B[0;0H'); //Limpa console no DART para melhor aparencia.
       print(
-          '\n### Digite 1 para SAIR! ### \n### Digite 2 para ver a LISTA \n### Para REMOVER Item da lista digite 3\n');
+          '### Digite 1 para SAIR! ### \n### Digite 2 para ver a LISTA \n### Digite 3 para REMOVER Item da lista ');
     }
   }
 }
